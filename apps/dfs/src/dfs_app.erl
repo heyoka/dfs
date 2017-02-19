28855749e6b5f93b1b506f69423ead8ee0b5b100
Alex Minichmair <alexander.minichmair@linemetrics.com>
2017-02-19 19:43:47 +0100

    rebar3 compatible app structure

---
+++ apps/dfs/src/dfs_app.erl
@@ -0,0 +1,11 @@
+%% Date: 19.02.17 - 19:37
+%% Ⓒ 2017 LineMetrics GmbH
+-module(dfs_app).
+-author("Alexander Minichmair").
+
+-behavior(application).
+%% API
+-export([start/0]).
+
+start() ->
+   ok.
